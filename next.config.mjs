db0927f64baffc203f9bc6464ff4ab8408a5b907
Zulker Nien <zulkerb9b@gmail.com>
2024-04-25 14:59:36 +0600

    Adding more url whitelists

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.tools.unlayer.com"],
+    domains: ["cdn.tools.unlayer.com", "fonts.gstatic.com"],
   },
 };
 
