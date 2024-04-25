baf0ea13f069ab3cedeb1e68a989815092bc28aa
Zulker Nien <zulkerb9b@gmail.com>
2024-04-25 15:59:00 +0600

    Testing without template generator

--- src/app/home/campaigns/template-generator/page.tsx
+++ src/app/home/campaigns/template-generator/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 import React, { useRef } from "react";
-import EmailEditor, { EditorRef, EmailEditorProps } from "react-email-editor";
+// import EmailEditor, { EditorRef, EmailEditorProps } from "react-email-editor";
 import "./template.css";
 const TemplateGenerator = () => {
-  const emailEditorRef = useRef<EditorRef>(null);
-  const exportHtml = () => {
-    const unlayer = emailEditorRef.current?.editor;
+  // const emailEditorRef = useRef<EditorRef>(null);
+  // const exportHtml = () => {
+  //   const unlayer = emailEditorRef.current?.editor;
 
-    unlayer?.exportHtml((data) => {
-      const { design, html } = data;
-      console.log("exportHtml", html);
-    });
-  };
-  const onReady: EmailEditorProps["onReady"] = (unlayer) => {};
+  //   unlayer?.exportHtml((data) => {
+  //     const { design, html } = data;
+  //     console.log("exportHtml", html);
+  //   });
+  // };
+  // const onReady: EmailEditorProps["onReady"] = (unlayer) => {};
   return (
     <div className="h-full flex flex-col gap-4 overflow-hidden rounded-md">
       <div className="flex items-center justify-end gap-4 ">
@@ -23,7 +23,7 @@ const TemplateGenerator = () => {
           Export HTML
         </button>
       </div>
-      <EmailEditor ref={emailEditorRef} onReady={onReady} />
+      {/* <EmailEditor ref={emailEditorRef} onReady={onReady} /> */}
     </div>
   );
 };
