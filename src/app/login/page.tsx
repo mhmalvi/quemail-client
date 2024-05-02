165e1f4f1619d3f3014b26efba8488e795f334f8
ZulkerQQ <zulker@quadque.tech>
2024-05-02 11:07:55 +0600

    testing google on production

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -69,7 +69,7 @@ const Login = () => {
 
   const handleGoogleClick = async () => {
     signIn("google", {
-      callbackUrl: "/",
+      callbackUrl: "https://www.quemailer.com",
     });
   };
 
