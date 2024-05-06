220f4436c19d7e28ae328e2edc231dc19079bfee
ZulkerQQ <zulker@quadque.tech>
2024-05-06 08:49:41 +0600

    fixed undefined window issue

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 const Authenticate = () => {
@@ -8,14 +8,16 @@ const Authenticate = () => {
   const email = searchParams.get("email");
   const id = searchParams.get("id");
   const photo = searchParams.get("photo");
-  
+
   return (
     <div className="h-screen w-screen flex items-center justify-center">
-      <h1>Welcome to Quemailer</h1>
-      <h1>UserName: {userName}</h1>
-      <h1>Email: {email}</h1>
-      <h1>Id: {id}</h1>
-      <h1>Photo: {photo}</h1>
+      <Suspense fallback={null}>
+        <h1>Welcome to Quemailer</h1>
+        <h1>UserName: {userName}</h1>
+        <h1>Email: {email}</h1>
+        <h1>Id: {id}</h1>
+        <h1>Photo: {photo}</h1>
+      </Suspense>
     </div>
   );
 };
