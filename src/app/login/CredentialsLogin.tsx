7d3ab8678a45098f5d09836f2a51cb31329ad9c1
ZulkerQQ <zulker@quadque.tech>
2024-05-06 15:15:38 +0600

    Feat: Added package lock to gitignore

--- src/app/login/CredentialsLogin.tsx
+++ src/app/login/CredentialsLogin.tsx
@@ -1,9 +1,11 @@
 "use client";
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
 import { verifyOTP } from "../api/auth";
 import { OTPData } from "@/components/utils/types";
 import { Storage } from "@/store/store";
 import { useRouter } from "next/navigation";
+import { Spinner } from "flowbite-react";
+import { successNotification } from "@/components/utils/toast";
 
 const CredentialsLogin = ({
   setCredentialsData,
@@ -13,8 +15,9 @@ const CredentialsLogin = ({
   credentialsData: OTPData;
 }) => {
   const router = useRouter();
-
+  const [buttonClick, setButtonClick] = useState(false);
   const handleCredentialSubmit = async () => {
+    setButtonClick(true);
     const response = await verifyOTP(credentialsData);
 
     if (response.status === 200) {
@@ -22,6 +25,8 @@ const CredentialsLogin = ({
       Storage.setItem("email", response.user.email);
       Storage.setItem("photo", response.user.photo);
       Storage.setItem("token", response.user.token);
+      setButtonClick(false);
+      successNotification("Welcome to quemailer");
       router.push("/home");
     }
   };
@@ -41,15 +46,20 @@ const CredentialsLogin = ({
             className="bg-transparent rounded-md focus:outline-none focus:ring-0 focus:border-brand-color outline-none border border-slate-400 px-5 py-2"
           />
         </div>
-
-        <button
-          type="button"
-          disabled={credentialsData.email.length === 0}
-          onClick={handleCredentialSubmit}
-          className="disabled:opacity-20 rounded-md w-full px-4 py-2 bg-gradient-to-r from-brand-color to-button-color-2 text-slate-300"
-        >
-          Log In
-        </button>
+        {buttonClick ? (
+          <div className="flex items-center justify-center disabled:opacity-20 rounded-md w-full px-4 py-2 bg-gradient-to-r from-brand-color to-button-color-2 text-slate-300">
+            <Spinner aria-label="Loading" className="fill-black" />
+          </div>
+        ) : (
+          <button
+            type="button"
+            disabled={credentialsData.email.length === 0}
+            onClick={handleCredentialSubmit}
+            className="disabled:opacity-20 rounded-md w-full px-4 py-2 bg-gradient-to-r from-brand-color to-button-color-2 text-slate-300"
+          >
+            Log In
+          </button>
+        )}
       </form>
     </div>
   );
