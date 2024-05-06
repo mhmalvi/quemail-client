a1a4c76c5ac2284fc9557bc35e7801de009e9433
ZulkerQQ <zulker@quadque.tech>
2024-05-06 10:30:55 +0600

    Added token to localstorage and blocked home

--- src/app/home/campaigns/page.tsx
+++ src/app/home/campaigns/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 const Campaigns = () => {
-  return <div>Campaigns</div>;
+  return <>{typeof window !== "undefined" && <div>Campaigns</div>}</>;
 };
 export default Campaigns;
--- src/app/home/campaigns/template-generator/page.tsx
+++ src/app/home/campaigns/template-generator/page.tsx
@@ -7,19 +7,23 @@ const TemplateGenerator = () => {
     ssr: false,
   });
   return (
-    <div className="h-full flex flex-col gap-4 overflow-hidden rounded-md">
-      <div className="flex items-center justify-end gap-4 ">
-        <button className="px-4 py-2 bg-brand-color rounded-md">
-          Save Template
-        </button>
-        <button className="px-4 py-2 bg-brand-color rounded-md">
-          Export HTML
-        </button>
-      </div>
-      <div className="h-full flex items-center justify-center">
-        <Editor />
-      </div>
-    </div>
+    <>
+      {typeof window !== "undefined" && (
+        <div className="h-full flex flex-col gap-4 overflow-hidden rounded-md">
+          <div className="flex items-center justify-end gap-4 ">
+            <button className="px-4 py-2 bg-brand-color rounded-md">
+              Save Template
+            </button>
+            <button className="px-4 py-2 bg-brand-color rounded-md">
+              Export HTML
+            </button>
+          </div>
+          <div className="h-full flex items-center justify-center">
+            <Editor />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
