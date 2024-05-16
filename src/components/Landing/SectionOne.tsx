daafdd9b59d0e50ba645210f45147906cf63e5c1
ZulkerQQ <zulker@quadque.tech>
2024-05-16 09:20:29 +0600

    Updated url redirect on signout

--- src/components/Landing/SectionOne.tsx
+++ src/components/Landing/SectionOne.tsx
@@ -6,7 +6,7 @@ import Images from "../utils/images";
 const SectionOne = () => {
   return (
     <div className="w-full flex flex-col gap-4 items-center justify-center h-screen	">
-      <h1 className="m-0 px-0 py-2 lg:text-4xl text-3xl text-center flex flex-col gap-2 text-slate-300 lg:mt-8">
+      <h1 className="m-0 px-0 py-2 lg:text-4xl text-3xl text-center flex flex-col gap-2 text-slate-300 lg:mt-8 mt-32">
         Reach your leads with
         <span className="bg-clip-text text-transparent bg-gradient-to-r from-brand-color to-brand-color-2 py-1 font-semibold lg:text-5xl text-4xl">
           Email Marketing
