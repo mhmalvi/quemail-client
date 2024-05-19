cf25e4f728be549387143e0f8573f97ef839a163
ZulkerQQ <zulker@quadque.tech>
2024-05-19 16:59:12 +0600

    Added Fetch and Insert Templates

---
+++ src/app/api/template.ts
@@ -0,0 +1,53 @@
+export const saveTemplate = async (data: Object) => {
+  try {
+    const result = await fetch(
+      `https://backend.quemailer.com/api/template-save`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    if (result.ok) {
+      const responseData = await result.json();
+      return responseData;
+    } else {
+      console.error("Error:", result.statusText);
+      return null;
+    }
+  } catch (error: any) {
+    return error.response;
+  }
+};
+export const fetchTemplate = async () => {
+  const token = typeof window !== "undefined" && localStorage.getItem("token");
+  console.log(token);
+  const userID =
+    typeof window !== "undefined" && localStorage.getItem("userID");
+  try {
+    const result = await fetch(
+      `https://backend.quemailer.com/api/template-fetch`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${token}`,
+        },
+        body: JSON.stringify({
+          client_id: userID,
+        }),
+      }
+    );
+    if (result.ok) {
+      const responseData = await result.json();
+      return responseData;
+    } else {
+      console.error("Error:", result.statusText);
+      return null;
+    }
+  } catch (error: any) {
+    return error.response;
+  }
+};
--- src/app/home/campaigns/template-generator/MainEditor.tsx
+++ src/app/home/campaigns/template-generator/MainEditor.tsx
@@ -2,8 +2,21 @@
 import { themeStore } from "@/store/store";
 import React, { useRef, useEffect, useState } from "react";
 import EmailEditor, { EditorRef, EmailEditorProps } from "react-email-editor";
+import { saveTemplate, fetchTemplate } from "@/app/api/template";
+import {
+  successNotification,
+  warningNotification,
+} from "@/components/utils/utility";
 
 const MainEditor = () => {
+  const userID =
+    typeof window !== "undefined" && localStorage.getItem("userID");
+  const [data, setData] = useState({
+    template: "",
+    name: "",
+    user_id: userID,
+  });
+  const [saveClicked, setSaveClicked] = useState<boolean>(false);
   const themes = themeStore((state: any) => state.theme);
   const emailEditorRef = useRef<EditorRef>(null);
 
@@ -22,34 +35,72 @@ const MainEditor = () => {
   const onReady: EmailEditorProps["onReady"] = (unlayer) => {
     console.log("Editor is ready");
   };
-  const [saveClicked, setSaveClicked] = useState<boolean>(false);
+ 
   useEffect(() => {
     if (saveClicked) {
       const unlayer = emailEditorRef.current?.editor;
       if (unlayer) {
-        unlayer.exportHtml((design: any) => {
-          console.log("design", design);
-          // Here you can add additional code to handle the saved design,
-          // such as sending it to a server or saving it to local storage.
+        unlayer.exportHtml((data: any) => {
+          setData((prev) => ({
+            ...prev,
+            template: data.html,
+          }));
         });
       }
     }
   }, [saveClicked]);
 
+  useEffect(() => {
+    const onSave = async () => {
+      if (data.template) {
+        const res = await saveTemplate(data);
+        if (res.status === 201) {
+          successNotification(res.message);
+        } else if (res.status === 409) {
+          warningNotification(res.message);
+        } else if (res.status === 422) {
+          warningNotification(res.message);
+        } else {
+          warningNotification(res.message);
+        }
+        setSaveClicked(false);
+      }
+    };
+
+    if (saveClicked && data.template) {
+      onSave();
+    }
+  }, [data, data.template, saveClicked]);
+
   return (
-    <div className="h-full flex flex-col gap-4 overflow-hidden rounded-md">
-      <div className="flex items-center justify-end gap-4 ">
-        <button
-          className="px-4 py-2 bg-brand-color rounded-md"
-          onClick={() => {
-            setSaveClicked(!saveClicked);
-          }}
-        >
-          Save Template
-        </button>
-        <button className="px-4 py-2 bg-brand-color rounded-md">
-          Export HTML
-        </button>
+    <div className="h-full flex flex-col gap-4 overflow-hidden rounded-md ">
+      <div className="flex items-center justify-between w-full pl-2">
+        <div className="w-1/4">
+          <input
+            onChange={(e) => {
+              setData((prev) => ({
+                ...prev,
+                name: e.target.value,
+              }));
+            }}
+            placeholder="Enter template name here"
+            className="w-full px-4 py-1 rounded-md bg-transparent text-dark-black border dark:border-light-glass shadow-md dark:text-slate-300"
+          />
+        </div>
+        <div className="flex items-center justify-end gap-4 ">
+          <button
+            className="px-4 py-2 bg-brand-color rounded-md disabled:opacity-20"
+            onClick={() => {
+              setSaveClicked(true);
+            }}
+            disabled={data.name === ""}
+          >
+            Save Template
+          </button>
+          <button className="px-4 py-2 bg-brand-color rounded-md disabled:opacity-20">
+            Export HTML
+          </button>
+        </div>
       </div>
       <div className="h-full flex items-center justify-center">
         <EmailEditor
