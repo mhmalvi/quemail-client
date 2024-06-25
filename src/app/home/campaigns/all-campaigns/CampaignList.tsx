438f440b307958599fed3adcb0f161946dae19c6
ZulkerQQ <zulker@quadque.tech>
2024-06-25 11:58:00 +0600

    Updated Campaign List

--- src/app/home/campaigns/all-campaigns/CampaignList.tsx
+++ src/app/home/campaigns/all-campaigns/CampaignList.tsx
@@ -1,16 +1,22 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, SetStateAction, Dispatch } from "react";
 import { fetchCampaign } from "@/app/api/campaign";
 import { ListGroup, Pagination, Dropdown, Tooltip } from "flowbite-react";
-import {
-  CampaignListType,
-} from "@/components/utils/types";
+import { CampaignListType } from "@/components/utils/types";
 import { showCampaignStore } from "@/store/store";
-
-const CampaignList = () => {
+interface CampaignProps {
+  campaignName: string;
+  senderName: string;
+  senderEmail: string;
+  count: number;
+}
+interface CampaignDetailsInterface {
+  setCampaignDetails: Dispatch<SetStateAction<CampaignProps | null>>;
+}
+const CampaignList = ({ setCampaignDetails }: CampaignDetailsInterface) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [campaignsPerPage, setCampaignsPerPage] = useState<number | null>(8);
   const [totalPage, setTotalPage] = useState<number>(1);
+  const [campaignsPerPage, setCampaignsPerPage] = useState<number | null>(8);
   const [idClicked, setIdClicked] = useState<number>();
 
   const campaignList = showCampaignStore((state) => state.campaignList);
@@ -20,9 +26,17 @@ const CampaignList = () => {
   );
 
   useEffect(() => {
+    const calculateCampaignsPerPage = () => {
+      const viewportHeight = window.innerHeight;
+      const campaignHeight = 62;
+      const campaignsCount = Math.ceil(viewportHeight / campaignHeight);
+      return campaignsCount;
+    };
     const userIDString =
       typeof window !== "undefined" && localStorage.getItem("userID");
     const userID = userIDString ? parseInt(userIDString, 10) : null;
+    const campaignsPerPage = calculateCampaignsPerPage();
+    setCampaignsPerPage(campaignsPerPage);
     const data = {
       userID: userID,
       page: currentPage,
@@ -41,82 +55,26 @@ const CampaignList = () => {
     })();
   }, [campaignsPerPage, currentPage, setCampaignList]);
 
-  const handlePerPage = (e: any) => {
-    const value = Number(e.currentTarget.textContent);
-    setCampaignsPerPage(value);
-    setCurrentPage(1)
-  };
-
   return (
     <>
       <div className="flex items-center justify-between text-dark-black dark:text-slate-300 dark:border-slate-300 border-violet-50 ">
         <h1 className="w-full text-dark-black dark:text-slate-300">
           Campaign List
         </h1>
-        <div className="w-full flex items-center justify-end">
-          <Dropdown
-            label="Campaigns to show"
-            dismissOnClick={true}
-            size="sm"
-            placement="bottom-end"
-            renderTrigger={() => (
-              <div className="flex items-center justify-center gap-4">
-                <span className="2xl:text-sm md:text-xs px-4 py-2 bg-brand-color rounded-md cursor-pointer text-white">
-                  Campaigns / page - {campaignsPerPage}
-                </span>
-              </div>
-            )}
-          >
-            <Dropdown.Item className="flex items-center justify-center w-full p-0">
-              <div className="w-full h-full p-2" onClick={handlePerPage}>
-                8
-              </div>
-            </Dropdown.Item>
-
-            <Dropdown.Item className="flex items-center justify-center w-full p-0">
-              <div className="w-full h-full p-2" onClick={handlePerPage}>
-                10
-              </div>
-            </Dropdown.Item>
-
-            <Dropdown.Item className="flex items-center justify-center w-full p-0">
-              <div className="w-full h-full p-2" onClick={handlePerPage}>
-                12
-              </div>
-            </Dropdown.Item>
-            <Dropdown.Item className="flex items-center justify-center w-full p-0">
-              <div className="w-full h-full p-2" onClick={handlePerPage}>
-                14
-              </div>
-            </Dropdown.Item>
-            <Dropdown.Item className="flex items-center justify-center w-full p-0">
-              <div className="w-full h-full p-2" onClick={handlePerPage}>
-                16
-              </div>
-            </Dropdown.Item>
-            <Dropdown.Item className="flex items-center justify-center w-full p-0">
-              <div className="w-full h-full p-2" onClick={handlePerPage}>
-                18
-              </div>
-            </Dropdown.Item>
-            <Dropdown.Item className="flex items-center justify-center w-full p-0">
-              <div className="w-full h-full p-2" onClick={handlePerPage}>
-                20
-              </div>
-            </Dropdown.Item>
-          </Dropdown>
-        </div>
       </div>
       <ListGroup className="relative flex flex-col w-full bg-dark-glass overflow-auto">
         <ListGroup.Item className="sticky top-0 bg-dark-black w-full flex items-center justify-between gap-2 p-0 m-0">
-          <p className="w-2/5 m-0 p-0 text-xs lg:text-sm border-r text-slate-300">
+          <p className="w-1/4 m-0 p-0 text-xs lg:text-sm border-r text-slate-300">
             Campaign Name
           </p>
-          <p className="w-1/5 m-0 p-0 text-xs lg:text-sm border-r text-slate-300">
+          <p className="w-1/4 m-0 p-0 text-xs lg:text-sm border-r text-slate-300">
+            Sender Name
+          </p>
+          <p className="w-1/4 m-0 p-0 text-xs lg:text-sm border-r text-slate-300">
+            Sender Email
+          </p>
+          <p className="w-1/4 m-0 p-0 text-xs lg:text-sm text-slate-300">
             No. of recipients
-          </p>
-          <p className="w-2/5 m-0 p-0 text-xs lg:text-sm text-slate-300">
-            Campaign Name
           </p>
         </ListGroup.Item>
         {campaignList?.campaigns !== null &&
@@ -128,11 +86,17 @@ const CampaignList = () => {
                     active={idClicked === index}
                     onClick={() => {
                       setIdClicked(index);
-                      setClickedCampaignId(item.id)
+                      setClickedCampaignId(item.id);
+                      setCampaignDetails({
+                        campaignName: item.campaignName,
+                        senderName: item.fromName,
+                        senderEmail: item.fromMail,
+                        count: item.count,
+                      });
                     }}
                     className="text-base flex items-center justify-center w-full gap-2"
                   >
-                    <div className="w-2/5 m-0 p-0 text-xs lg:text-sm truncate flex items-center xl:justify-center justify-start">
+                    <div className="w-1/4 m-0 p-0 text-xs lg:text-sm truncate flex items-center justify-center">
                       <Tooltip
                         content={item.campaignName}
                         className="bg-brand-color"
@@ -141,10 +105,16 @@ const CampaignList = () => {
                         {item.campaignName}
                       </Tooltip>
                     </div>
-                    <p className="w-1/5 m-0 p-0 text-xs lg:text-sm">
-                      {item.count}
-                    </p>
-                    <div className="w-2/5 m-0 p-0 text-xs lg:text-sm truncate flex items-center xl:justify-center justify-start">
+                    <div className="w-1/4 m-0 p-0 text-xs lg:text-sm truncate flex items-center justify-center">
+                      <Tooltip
+                        content={item.fromMail}
+                        className="bg-brand-color"
+                        placement="bottom"
+                      >
+                        {item.fromName}
+                      </Tooltip>
+                    </div>
+                    <div className="w-1/4 m-0 p-0 text-xs lg:text-sm truncate flex items-center justify-center">
                       <Tooltip
                         content={item.fromMail}
                         className="bg-brand-color"
@@ -153,6 +123,9 @@ const CampaignList = () => {
                         {item.fromMail}
                       </Tooltip>
                     </div>
+                    <p className="w-1/4 m-0 p-0 text-xs lg:text-sm">
+                      {item.count}
+                    </p>
                   </ListGroup.Item>
                 </div>
               );
--- src/app/home/campaigns/all-campaigns/page.tsx
+++ src/app/home/campaigns/all-campaigns/page.tsx
@@ -3,15 +3,26 @@ import { CONTAINER_STYLES } from "@/components/styles/flex_container";
 import React, { useEffect, useState } from "react";
 import CampaignList from "./CampaignList";
 import CampaignItems from "./CampaignItems";
+import { showCampaignStore } from "@/store/store";
 
 const AllCampaigns = () => {
+  const clickedCampaignId = showCampaignStore(
+    (state) => state.clickedCampaignId
+  );
+  const [campaignDetails, setCampaignDetails] = useState<{
+    campaignName: string;
+    senderName: string;
+    senderEmail: string;
+    count: number;
+  } | null>(null);
   return (
     <div className={CONTAINER_STYLES}>
-      <div className="relative w-1/3 h-full rounded-md flex flex-col gap-4 overflow-hidden">
-        <CampaignList />
-      </div>
-      <div className="relative w-2/3 h-full dark:bg-dark-glass bg-violet-50 rounded-md p-4 flex flex-col gap-8 overflow-hidden">
-        <CampaignItems />
+      <div className="relative w-full h-full rounded-md flex flex-col gap-4 overflow-hidden">
+        {clickedCampaignId !== null ? (
+          <CampaignItems campaignDetails={campaignDetails} />
+        ) : (
+          <CampaignList setCampaignDetails={setCampaignDetails} />
+        )}
       </div>
     </div>
   );
