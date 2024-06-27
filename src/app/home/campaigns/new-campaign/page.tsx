be694cc09bca484554582f304cf91fc935a86783
Zulker Nien <zulkerb9b@gmail.com>
2024-06-27 08:37:26 +0600

    Contact view now functional

--- src/app/home/campaigns/new-campaign/page.tsx
+++ src/app/home/campaigns/new-campaign/page.tsx
@@ -289,43 +289,7 @@ const NewCampaign = () => {
         <Scheduler />
       </div> */}
 
-      {/* <Modal
-        dismissible
-        show={viewRecipients}
-        onClose={() => setViewRecipients(false)}
-      >
-        <Modal.Header className="dark:bg-dark-glass bg-violet-50">
-          Selected Recipients
-        </Modal.Header>
-        <Modal.Body className="dark:bg-dark-black bg-violet-50 text-slate-300">
-          <Table className="w-full !h-20 overflow-y-scroll">
-            <Table.Head className="sticky top-0 py-0 !rounded-tl-md w-full">
-              <Table.HeadCell className="sticky top-0 py-2">
-                Name
-              </Table.HeadCell>
-              <Table.HeadCell className="sticky top-0 py-2">
-                Email
-              </Table.HeadCell>
-              <Table.HeadCell className="sticky top-0 py-2">
-                Group
-              </Table.HeadCell>
-            </Table.Head>
-            <Table.Body className="divide-y overflow-auto">
-              {newCampaign?.recipient !== null &&
-                newCampaign?.recipient.map((item: any, index: number) => (
-                  <Table.Row
-                    key={index}
-                    className="dark:border-gray-700 dark:bg-gray-800 w-full"
-                  >
-                    <Table.Cell className="w-1/3">{item.json.name}</Table.Cell>
-                    <Table.Cell className="w-1/3">{item.json.email}</Table.Cell>
-                    <Table.Cell className="w-1/3">{item.json.group}</Table.Cell>
-                  </Table.Row>
-                ))}
-            </Table.Body>
-          </Table>
-        </Modal.Body>
-      </Modal> */}
+      
     </div>
   );
 };
