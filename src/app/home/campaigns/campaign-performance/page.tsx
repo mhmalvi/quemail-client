223b531247f6ba66749cfc95ee3d4422a9418097
ZulkerQQ <zulker@quadque.tech>
2024-06-28 17:02:32 +0600

    Feat: Add contact manuallly

--- src/app/home/campaigns/campaign-performance/page.tsx
+++ src/app/home/campaigns/campaign-performance/page.tsx
@@ -1,22 +1,35 @@
 "use client";
+import { useState } from "react";
 import Chart from "react-apexcharts";
 const CampaignPerformance = () => {
-  const options = {
-
-  }
+  const [state, setState] = useState({
+    options: {
+      chart: {
+        id: "basic-bar",
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+      },
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91],
+      },
+    ],
+  });
   return (
     <div className="w-full h-full flex items-center justify-center gap-4 dark:bg-dark-glass shadow-md backdrop-blur-2xl bg-white rounded-md p-4 overflow-hidden">
-      This feature is under development
-      {/* <div className="row">
-          <div className="mixed-chart">
-            <Chart
-              options={this.state.options}
-              series={this.state.series}
-              type="bar"
-              width="500"
-            />
-          </div>
-        </div> */}
+      {/* This feature is under development */}
+      <div className="row">
+
+          <Chart
+            options={state.options}
+            series={state.series}
+            type="bar"
+            width="500"
+          />
+      </div>
     </div>
   );
 };
