d60fa4f27e1a4fd448fbaca526787196d6eacbe8
ZulkerQQ <zulker@quadque.tech>
2024-06-28 17:05:03 +0600

    Feat: Add contact manuallly

--- src/app/home/campaigns/campaign-performance/page.tsx
+++ src/app/home/campaigns/campaign-performance/page.tsx
@@ -2,26 +2,26 @@
 import { useState } from "react";
 import Chart from "react-apexcharts";
 const CampaignPerformance = () => {
-  const [state, setState] = useState({
-    options: {
-      chart: {
-        id: "basic-bar",
-      },
-      xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-      },
-    },
-    series: [
-      {
-        name: "series-1",
-        data: [30, 40, 45, 50, 49, 60, 70, 91],
-      },
-    ],
-  });
+  // const [state, setState] = useState({
+  //   options: {
+  //     chart: {
+  //       id: "basic-bar",
+  //     },
+  //     xaxis: {
+  //       categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+  //     },
+  //   },
+  //   series: [
+  //     {
+  //       name: "series-1",
+  //       data: [30, 40, 45, 50, 49, 60, 70, 91],
+  //     },
+  //   ],
+  // });
   return (
     <div className="w-full h-full flex items-center justify-center gap-4 dark:bg-dark-glass shadow-md backdrop-blur-2xl bg-white rounded-md p-4 overflow-hidden">
-      {/* This feature is under development */}
-      <div className="row">
+      This feature is under development
+      {/* <div className="row">
 
           <Chart
             options={state.options}
@@ -29,7 +29,7 @@ const CampaignPerformance = () => {
             type="bar"
             width="500"
           />
-      </div>
+      </div> */}
     </div>
   );
 };
