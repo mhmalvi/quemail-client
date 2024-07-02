aba0e574bd7dd4601749754e0d5bf54feeb4d98b
Doha Khan <doha123khan@gmail.com>
2024-07-02 14:03:08 +0600

    Dynamic account details

--- src/app/home/profile/my-profile/AccountDetails.tsx
+++ src/app/home/profile/my-profile/AccountDetails.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Storage } from "@/store/store";
 
 const AccountDetails = () => {
-  const [editClicked, setEditClicked] = useState(false);
+  //const [editClicked, setEditClicked] = useState(false);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
@@ -30,7 +30,7 @@ const AccountDetails = () => {
     <div className="relative dark:border-none border w-full h-full dark:bg-dark-glass bg-violet-50 shadow-md background-blur-2xl rounded-md xl:p-4 flex flex-col xl:gap-4 gap-0 overflow-hidden">
       <div className="flex items-center xl:h-20 h-16 justify-between p-4">
         <h1 className="m-0 p-0 text-lg dark:text-slate-300 text-dark-black">Account Details</h1>
-        {editClicked ? (
+        {/* {editClicked ? (
           <div className="flex items-center justify-end gap-4">
             <button onClick={()=>{
              setUserInfo()
@@ -56,48 +56,48 @@ const AccountDetails = () => {
           >
             Edit
           </button>
-        )}
+        )} */}
       </div>
       <div className="flex flex-col w-full p-4 xl:gap-4 gap-2">
         <div className="flex flex-col justify-center">
           <label className="xl:text-base text-sm dark:text-slate-300 text-dark-black">First Name</label>
-          {editClicked ? (
+          {/* {editClicked ? (
             <input
             className="xl:px-4 px-2 xl:py-2 py-1 bg-transparent dark:text-slate-300 text-dark-black rounded-md border border-brand-color focus:ring-0 focus:outline-none active:outline-none active:ring-0"
             value={firstName}
             onChange={handleChangeFirst}
           />
-          ) : (
+          ) : ( */}
             <h1 className="text-brand-color font-semibold xl:px-4 px-2 xl:py-2 py-1 xl:text-base text-sm">
               {Storage.getItem("userName")}
             </h1>
-          )}
+          {/* )} */}
         </div>
         <div className="flex flex-col justify-center">
           <label className="xl:text-base text-sm dark:text-slate-300 text-dark-black">Last Name</label>
-          {editClicked ? (
+          {/* {editClicked ? (
             <input className=" dark:text-slate-300 text-dark-black xl:px-4 px-2 xl:py-2 py-1 bg-transparent rounded-md border border-brand-color focus:ring-0 focus:outline-none active:outline-none active:ring-0"
             value={lastName}
             onChange={handleChangeLast}
              />
-          ) : (
+          ) : ( */}
             <h1 className="text-brand-color font-semibold xl:px-4 px-2 xl:py-2 py-1 m-0 xl:text-base text-sm">
               {Storage.getItem("userName")}
             </h1>
-          )}
+          {/* )} */}
         </div>
         <div className="flex flex-col justify-center">
           <label className="xl:text-base text-sm dark:text-slate-300 text-dark-black">Email Address</label>
-          {editClicked ? (
+          {/* {editClicked ? (
             <input className=" dark:text-slate-300 text-dark-black xl:px-4 px-2 xl:py-2 py-1 bg-transparent rounded-md border border-brand-color focus:ring-0 focus:outline-none active:outline-none active:ring-0"
             value={email}
             onChange={handleChangeEmail}
              />
-          ) : (
+          ) : ( */}
             <h1 className="text-brand-color font-semibold xl:px-4 px-2 xl:py-2 py-1 m-0 xl:text-base text-sm">
               {Storage.getItem("email")}
             </h1>
-          )}
+          {/* )} */}
         </div>
       </div>
     </div>
