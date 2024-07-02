933e993d2a64a89fec9b65552bbe64f06ac8da43
Doha Khan <doha123khan@gmail.com>
2024-07-02 13:54:31 +0600

    Dynamic account details

--- src/app/home/profile/my-profile/page.tsx
+++ src/app/home/profile/my-profile/page.tsx
@@ -13,11 +13,8 @@ const MyProfile = () => {
       <div className="h-1/4 dark:bg-dark-glass bg-violet-50 shadow-md dark:border-none border w-full rounded-md flex items-center justify-between gap-4 p-8">
         <div className="flex flex-col gap-4">
           <h1 className="m-0 p-0 xl:text-4xl text-2xl dark:text-slate-300 text-dark-black">
-            John Doe
+            {Storage.getItem("userName")}
           </h1>
-          <p className="m-0 xl:px-4 xl:py-2 px-2 py-1 xl:text-base text-sm bg-light-black rounded-md text-center">
-            Profile 01
-          </p>
         </div>
         <div className="h-full flex items-center xl:gap-8 gap-4">
           <div className="rounded-full xl:h-32 h-20 xl:w-32 w-20 overflow-hidden">
