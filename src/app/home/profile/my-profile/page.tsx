d7cd17081372b1b66955d6269890120fd6052395
Doha Khan <doha123khan@gmail.com>
2024-07-02 14:43:02 +0600

    Dynamic account details

--- src/app/home/profile/my-profile/page.tsx
+++ src/app/home/profile/my-profile/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import Images from "@/components/utils/images";
 
 const MyProfile = () => {
-  const photo = Storage.getItem("photo");
+  const photo = Storage.getItem("photo"); 
   return (
     <div className="relative w-full h-full rounded-md flex flex-col gap-4 overflow-hidden">
       <div className="h-1/4 dark:bg-dark-glass bg-violet-50 shadow-md dark:border-none border w-full rounded-md flex items-center justify-between gap-4 p-8">
