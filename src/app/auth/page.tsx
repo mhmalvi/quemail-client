39218cbc846ad76d71d8b256195c266917e23ab4
ZulkerQQ <zulker@quadque.tech>
2024-07-11 15:30:15 +0600

    fix: updated filter and download bugs

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -6,21 +6,32 @@ import { Storage } from "@/store/store";
 
 const Authenticate = () => {
   const router = useRouter();
-  
+
   const searchParams = useSearchParams();
   const userName = searchParams.get("userName");
   const email = searchParams.get("email");
   const photo = searchParams.get("photo");
   const token = searchParams.get("token");
   const userID = searchParams.get("userID");
-  const customerID = searchParams.get("customerID");
+  const subscription = searchParams.get("subscription");
+  const first_user = searchParams.get("first_user");
 
-  if (token && userName && email && photo && userID && customerID) {
+  if (
+    token &&
+    userName &&
+    email &&
+    photo &&
+    userID &&
+    first_user &&
+    subscription
+  ) {
     Storage.setItem("userName", userName);
     Storage.setItem("email", email);
     Storage.setItem("photo", photo);
     Storage.setItem("token", token);
     Storage.setItem("userID", Number(userID));
+    Storage.setItem("subscription", subscription);
+    Storage.setItem("first_user", first_user);
     router.push("/home");
   }
 
