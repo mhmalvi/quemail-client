bf3a5627760ea9480d76512ddd65785cbc0e7105
ZulkerQQ <zulker@quadque.tech>
2024-07-11 13:52:16 +0600

    fix: updated design

--- src/app/home/campaigns/compare-email-performance/CompareBox.tsx
+++ src/app/home/campaigns/compare-email-performance/CompareBox.tsx
@@ -12,9 +12,7 @@ const CompareBox: React.FC<FilterProps> = ({ position }) => {
     (state) => state.campaignDetails2
   );
 
-  const winnerCampaign = compareCampaignStore(
-    (state) => state.winnerCampaign
-  );
+  const winnerCampaign = compareCampaignStore((state) => state.winnerCampaign);
 
   const [state, setState] = useState<any>(null);
 
@@ -35,81 +33,72 @@ const CompareBox: React.FC<FilterProps> = ({ position }) => {
     if (field === "bounce" && leftValue != null && rightValue != null) {
       if (leftValue < rightValue) {
         return state === leftCampaignDetails
-          ? "text-green-500 dark:text-green-800"
-          : "text-red-500 dark:text-red-800";
+          ? "text-green-500 dark:text-green-500"
+          : "text-red-500 dark:text-red-500";
       } else if (leftValue > rightValue) {
         return state === rightCampaignDetails
-          ? "text-green-500 dark:text-green-800"
-          : "text-red-500 dark:text-red-800";
+          ? "text-green-500 dark:text-green-500"
+          : "text-red-500 dark:text-red-500";
       }
     }
 
     if (field != "bounce" && leftValue !== null && rightValue !== null) {
       if (leftValue > rightValue) {
         return state === leftCampaignDetails
-          ? "text-green-500 dark:text-green-800"
-          : "text-red-500 dark:text-red-800";
+          ? "text-green-500 dark:text-green-500"
+          : "text-red-500 dark:text-red-500";
       } else if (leftValue < rightValue) {
         return state === rightCampaignDetails
-          ? "text-green-500 dark:text-green-800"
-          : "text-red-500 dark:text-red-800";
+          ? "text-green-500 dark:text-green-500"
+          : "text-red-500 dark:text-red-500";
       }
     }
     return "text-dark-black dark:text-slate-300";
   };
 
   const winnerDiv = () => {
-    if ( position === "left" && winnerCampaign === "left"){
-        return "bg-green-200 dark:bg-green-200/70"
+    if (position === "left" && winnerCampaign === "left") {
+      return "border-4 border-green-500 dark:border-green-500";
     }
-    if ( position === "left" && winnerCampaign === "right"){
-        return "bg-red-200 dark:bg-red-200/70"
+    if (position === "left" && winnerCampaign === "right") {
+      return "border-4 border-red-500 dark:border-red-500";
     }
-    if ( position === "right" && winnerCampaign === "right"){
-        return "bg-green-200 dark:bg-green-200/70 "
+    if (position === "right" && winnerCampaign === "right") {
+      return "border-4 border-green-500 dark:border-green-500";
     }
-    if ( position === "right" && winnerCampaign === "left"){
-        return "bg-red-200 dark:bg-red-200/70"
+    if (position === "right" && winnerCampaign === "left") {
+      return "border-4 border-red-500 dark:border-red-500";
     }
-    return "bg-violet-200 dark:bg-dark-black"
-  }
+    return "bg-violet-200 dark:bg-light-glass";
+  };
 
   return (
     <div
-      className={`duration-100 ease-in-out border border-violet-200 dark:border-light-glass h-full rounded-md flex flex-col items-center justify-center p-4 ${winnerDiv()}`}
+      className={`duration-100 ease-in-out border h-full rounded-md flex flex-col gap-6 items-center justify-center p-4 ${winnerDiv()}`}
     >
-      <div className="h-full m-5 w-full p-4 rounded-md flex flex-col items-center gap-4">
-        <div className="h-1/3 w-2/3 overflow-hidden flex flex-col rounded-md items-center justify-center">
-          <Tooltip content="Hovered over" className="bg-brand-color">
-            <h1 className="text-dark-black dark:text-slate-300 text-4xl font-semibold">
-              33
-            </h1>
-          </Tooltip>
-        </div>
-        <div className="h-1/3 w-2/3 overflow-hidden flex flex-col rounded-md items-center justify-center">
-          <Tooltip content="Hovered over" className="bg-brand-color">
-            <h1 className={`text-4xl font-semibold ${getTextColor("open")}`}>
-              {state?.open}
-            </h1>
-          </Tooltip>
-        </div>
-        <div className="h-1/3 w-2/3 overflow-hidden flex flex-col rounded-md items-center justify-center">
-          <Tooltip content="Hovered over" className="bg-brand-color">
-            <h1
-              className={`text-4xl font-semibold ${getTextColor("subscribed")}`}
-            >
-              {state?.subscribed}
-            </h1>
-          </Tooltip>
-        </div>
-        <div className="h-1/3 w-2/3 overflow-hidden flex flex-col rounded-md items-center justify-center">
-          <Tooltip content="Hovered over" className="bg-brand-color">
-            <h1 className={`text-4xl font-semibold ${getTextColor("bounce")}`}>
-              {state?.bounce}
-            </h1>
-          </Tooltip>
-        </div>
-      </div>
+      <Tooltip content="Hovered over" className="bg-brand-color">
+        <h1 className="text-dark-black dark:text-slate-300 text-4xl font-semibold p-0 m-0">
+          33
+        </h1>
+      </Tooltip>
+
+      <Tooltip content="Hovered over" className="bg-brand-color">
+        <h1 className={`text-4xl font-semibold ${getTextColor("open")} p-0 m-0`}>
+          {state?.open}
+        </h1>
+      </Tooltip>
+
+      <Tooltip content="Hovered over" className="bg-brand-color">
+        <h1 className={`text-4xl font-semibold ${getTextColor("subscribed")} p-0 m-0`}>
+          {state?.subscribed}
+        </h1>
+      </Tooltip>
+
+      <Tooltip content="Hovered over" className="bg-brand-color">
+        <h1 className={`text-4xl font-semibold ${getTextColor("bounce")} p-0 m-0`}>
+          {state?.bounce}
+        </h1>
+      </Tooltip>
     </div>
   );
 };
--- src/app/home/campaigns/compare-email-performance/page.tsx
+++ src/app/home/campaigns/compare-email-performance/page.tsx
@@ -4,63 +4,53 @@ import Filter from "./Filter";
 import CompareBox from "./CompareBox";
 
 const CompareEmailCampaign = () => {
-  const [data1, setData1] = useState(null);
-  const [data2, setData2] = useState(null);
-
-  const handleFetchCampaign = async (id:any, setData:any) => {
-    try {
-      const response = await fetch(`https://backend.quemailer.com/campaigns/${id}`);
-      const data = await response.json();
-      setData(data);
-    } catch (error) {
-      console.error("Error fetching campaign data:", error);
-    }
-  };
 
   return (
     <div className="w-full h-full flex justify-center gap-4 dark:bg-dark-glass shadow-md backdrop-blur-2xl bg-white rounded-md p-4 overflow-hidden">
-      <div className = "flex flex-col w-1/4 gap-4">
-          <Filter position="left" />
-          <CompareBox position="left" />
-      </div>
-      <div className="w-1/4 flex flex-col justify-between justify-center items-center pt-32 pb-20">
-      <div className="flex flex-row items-center w-full">
-        <hr className="flex-grow border-violet-300 border-t-2" />
-        <h1 className="px-4 text-xl font-semibold text-dark-black dark:text-slate-300">
-          Clicked
-        </h1>
-        <hr className="flex-grow border-violet-300 border-t-2" />
+      <div className="flex flex-col w-1/4 gap-4">
+        <Filter position="left" />
+        <CompareBox position="left" />
       </div>
 
-      <div className="flex flex-row items-center w-full">
-        <hr className="flex-grow border-violet-300 border-t-2" />
-        <h1 className="px-4 text-xl font-semibold text-dark-black dark:text-slate-300">
-          Opened
-        </h1>
-        <hr className="flex-grow border-violet-300 border-t-2" />
+      <div className="w-1/4 h-full flex flex-col justify-end items-center pt-14">
+        <div className="w-full h-full flex flex-col justify-center gap-9 items-center border-4 border-transparent">
+          <div className="flex flex-row items-center w-full">
+            <hr className="flex-grow border-violet-300 dark:border-light-glass border-t-2" />
+            <h1 className="px-4 text-xl font-semibold text-dark-black dark:text-slate-300">
+              Clicked
+            </h1>
+            <hr className="flex-grow border-violet-300 dark:border-light-glass border-t-2" />
+          </div>
+          <div className="flex flex-row items-center w-full">
+            <hr className="flex-grow border-violet-300 dark:border-light-glass border-t-2" />
+            <h1 className="px-4 text-xl font-semibold text-dark-black dark:text-slate-300">
+              Opened
+            </h1>
+            <hr className="flex-grow border-violet-300 dark:border-light-glass border-t-2" />
+          </div>
+          <div className="flex flex-row items-center w-full">
+            <hr className="flex-grow border-violet-300 dark:border-light-glass border-t-2" />
+            <h1 className="px-4 text-xl font-semibold text-dark-black dark:text-slate-300">
+              Subscribed
+            </h1>
+            <hr className="flex-grow border-violet-300 dark:border-light-glass border-t-2" />
+          </div>
+          <div className="flex flex-row items-center w-full">
+            <hr className="flex-grow border-violet-300 dark:border-light-glass border-t-2" />
+            <h1 className="px-4 text-xl font-semibold text-dark-black dark:text-slate-300">
+              Bounced
+            </h1>
+            <hr className="flex-grow border-violet-300 dark:border-light-glass border-t-2" />
+          </div>
+        </div>
       </div>
-      <div className="flex flex-row items-center w-full">
-        <hr className="flex-grow border-violet-300 border-t-2" />
-        <h1 className="px-4 text-xl font-semibold text-dark-black dark:text-slate-300">
-          Subscribed
-        </h1>
-        <hr className="flex-grow border-violet-300 border-t-2" />
-      </div>
-      <div className="flex flex-row items-center w-full">
-        <hr className="flex-grow border-violet-300 border-t-2" />
-        <h1 className="px-4 text-xl font-semibold text-dark-black dark:text-slate-300">
-          Bounced
-        </h1>
-        <hr className="flex-grow border-violet-300 border-t-2" />
-      </div>
-      </div>
-
-      <div className = "flex flex-col w-1/4 gap-4">
-          <Filter position={"right"} />
-          <CompareBox position="right"/>
+      <div className="flex flex-col w-1/4 gap-4">
+        <Filter position={"right"} />
+        <CompareBox position="right" />
       </div>
     </div>
   );
 };
+// 2xl:gap-24 xl:gap-16 gap-8
 
 export default CompareEmailCampaign;
