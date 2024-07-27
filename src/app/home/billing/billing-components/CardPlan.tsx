6362c4c4ca7c84cdbb492b14bd7726fc91f995f0
DohaKhanMozlish <doha@quadque.tech>
2024-07-27 14:55:53 +0600

    price ID from otp

--- src/app/home/billing/billing-components/CardPlan.tsx
+++ src/app/home/billing/billing-components/CardPlan.tsx
@@ -1,7 +1,16 @@
+import { Storage } from "@/store/store";
+
 const CardPlan = () => {
+  const priceID = Storage.getItem("priceID");
+
   return (
     <div className="step-3 summary-element w-full bg-white dark:bg-light-glass backdrop-blur-xl dark:border-none border border-violet-200 rounded-md overflow-hidden p-4">
-      <h1 className="xl:text-xl text-base m-0 p-0 dark:text-white text-dark-black">
+      <h1
+        className="xl:text-xl text-base m-0 p-0 dark:text-white text-dark-black"
+        onClick={() => {
+          console.log(priceID);
+        }}
+      >
         Card Plan
       </h1>
     </div>
