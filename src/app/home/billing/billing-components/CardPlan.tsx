b260ed43e3964cd6541138e7bb1284596cd257e6
DohaKhanMozlish <doha@quadque.tech>
2024-07-29 09:27:21 +0600

    removed strip info from storage

--- src/app/home/billing/billing-components/CardPlan.tsx
+++ src/app/home/billing/billing-components/CardPlan.tsx
@@ -2,14 +2,15 @@ import { Storage } from "@/store/store";
 import { subscriptionDetails } from "@/app/api/billing";
 
 const CardPlan = () => {
-  const priceID = Storage.getItem("priceID");
-
+  const handleButton = () => {
+    subscriptionDetails();
+  };
   return (
     <div className="step-3 summary-element w-full bg-white dark:bg-light-glass backdrop-blur-xl dark:border-none border border-violet-200 rounded-md overflow-hidden p-4">
       <h1
         className="xl:text-xl text-base m-0 p-0 dark:text-white text-dark-black"
         onClick={() => {
-          console.log(priceID);
+          handleButton();
         }}
       >
         Card Plan
