b61f517ce37adeb7e984403c17a753ec01e4f7ff
DohaKhanMozlish <doha@quadque.tech>
2024-07-30 12:30:53 +0600

    added history card

--- src/app/home/billing/billing-components/CardPlan.tsx
+++ src/app/home/billing/billing-components/CardPlan.tsx
@@ -56,7 +56,7 @@ const CardPlan = () => {
   }, [staticEndTime]);
 
   return (
-    <div className="step-3 summary-element w-full bg-white dark:bg-light-glass backdrop-blur-xl dark:border-none border border-violet-500 rounded-md overflow-hidden p-4">
+    <div className="step-3 summary-element w-full bg-white dark:bg-light-glass backdrop-blur-xl dark:border-none border border-violet-200 rounded-md overflow-hidden p-4">
       <h1 className="xl:text-xl text-base m-0 p-0 dark:text-white text-dark-black text-center">
         Card Plan
       </h1>
