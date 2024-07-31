ae4d072d72ce5fc5587295c16b74a2e54cda6220
DohaKhanMozlish <doha@quadque.tech>
2024-07-31 15:57:40 +0600

    added lot of things

--- src/app/home/billing/billing-components/packageCard.tsx
+++ src/app/home/billing/billing-components/packageCard.tsx
@@ -18,9 +18,15 @@ const PackageCard: React.FC<PackageCardProps> = ({ packageName }) => {
       case "Enterprise":
         return "bg-gradient-to-r from-diamond-light to-diamond-dark";
       default:
-        return "bg-white";
+        return "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 transition-all duration-500";
     }
   };
+
+  //bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 - enterprise
+  //bg-gradient-to-r from-indigo-500 from-20% via-sky-500 via-40% to-emerald-500 to-90% - professional
+  //bg-gradient-to-r from-pink-500 from-10% via-red-500 via-30% to-yellow-500 to-90% - growth
+  //bg-gradient-to-r from-red-400 from-10% via-pink-500 via-30% to-purple-600 to-90% -
+  //bg-gradient-to-r from-gray-300 from-10% via-gray-400 via-30% to-gray-600 to-90% - free
 
   return (
     <div
@@ -28,7 +34,7 @@ const PackageCard: React.FC<PackageCardProps> = ({ packageName }) => {
         packageName
       )}`}
     >
-      <h1 className="flex text-base m-0 p-0 font-medium text-slate-600">
+      <h1 className="flex text-base m-0 p-0 font-medium text-slate-200">
         {packageName}
       </h1>
     </div>
