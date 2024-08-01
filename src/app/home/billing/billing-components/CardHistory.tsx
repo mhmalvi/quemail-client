e5df1385301bb997338f0ea37729237143f1e17c
DohaKhanMozlish <doha@quadque.tech>
2024-08-01 11:18:56 +0600

    updated Card History

--- src/app/home/billing/billing-components/CardHistory.tsx
+++ src/app/home/billing/billing-components/CardHistory.tsx
@@ -1,5 +1,5 @@
 import { stripeInvoiceHistory } from "@/app/api/billing";
-import { Table, Tooltip } from "flowbite-react";
+import { Spinner, Table, Tooltip } from "flowbite-react";
 import { Key, useEffect, useState } from "react";
 import { TbCaretLeftRight, TbDownload } from "react-icons/tb";
 import { Dropdown } from "flowbite-react";
@@ -52,154 +52,167 @@ const CardHistory = () => {
           <TbCaretLeftRight className="text-brand-color dark:text-white" />
         </button>
       </div>
-      <div className="w-full flex flex-row justify-center items-center gap-4">
-        <Dropdown
-          label="Groups ▼"
-          placement="bottom-start"
-          renderTrigger={() => (
-            <div className="px-2 py-1 rounded-md border border-brand-color cursor-pointer overflow-hidden">
-              <h1 className="flex items-center justify-center gap-4 duration-100 ease-in text-dark-black dark:text-slate-300 xl:text-base text-sm ">
-                {status} <span className="text-xs">▼</span>
-              </h1>
-            </div>
-          )}
-          className="dark:bg-dark-black bg-light-glass backdrop-blur-2xl border-none z-40"
-        >
-          <Dropdown.Item
-            className="dark:text-slate-300 text-light-black hover:text-slate-700"
-            onClick={() => {
-              handleStatus("paid");
-            }}
-          >
-            Paid
-          </Dropdown.Item>
-          <Dropdown.Item
-            className="dark:text-slate-300 text-light-black hover:text-slate-700"
-            onClick={() => {
-              handleStatus("open");
-            }}
-          >
-            Open
-          </Dropdown.Item>
-        </Dropdown>
-        <Dropdown
-          label="Groups ▼"
-          placement="bottom-start"
-          renderTrigger={() => (
-            <div className="px-2 py-1 rounded-md border border-brand-color cursor-pointer overflow-hidden">
-              <h1 className="flex items-center justify-center gap-4 duration-100 ease-in text-dark-black dark:text-slate-300 xl:text-base text-sm ">
-                {limit} <span className="text-xs">▼</span>
-              </h1>
-            </div>
-          )}
-          className="dark:bg-dark-black bg-light-glass backdrop-blur-2xl border-none z-40"
-        >
-          <Dropdown.Item
-            className="dark:text-slate-300 text-light-black hover:text-slate-700"
-            onClick={() => {
-              handleLimit(5);
-            }}
-          >
-            5
-          </Dropdown.Item>
-          <Dropdown.Item
-            className="dark:text-slate-300 text-light-black hover:text-slate-700"
-            onClick={() => {
-              handleLimit(10);
-            }}
-          >
-            10
-          </Dropdown.Item>
-          <Dropdown.Item
-            className="dark:text-slate-300 text-light-black hover:text-slate-700"
-            onClick={() => {
-              handleLimit(100);
-            }}
-          >
-            100
-          </Dropdown.Item>
-        </Dropdown>
-      </div>
-      <div className="w-full h-full overflow-auto">
-        <Table hoverable striped>
-          <Table.Head className="w-full">
-            {expanded ? (
-              <>
-                <Table.HeadCell className="w-1/4 sticky text-center">
-                  Date
-                </Table.HeadCell>
-                <Table.HeadCell className="w-1/4 sticky text-center">
-                  Amount
-                </Table.HeadCell>
-                <Table.HeadCell className="w-1/4 sticky text-center">
-                  Description
-                </Table.HeadCell>
-                <Table.HeadCell className="w-1/4 sticky text-center">
-                  Invoice
-                </Table.HeadCell>
-              </>
-            ) : (
-              <>
-                <Table.HeadCell className="w-1/2 sticky text-center">
-                  Date
-                </Table.HeadCell>
-                <Table.HeadCell className="w-1/2 sticky text-center">
-                  Amount
-                </Table.HeadCell>
-              </>
-            )}
-          </Table.Head>
-          <Table.Body className="divide-y">
-            {invoices &&
-              invoices.map((items: any, index: Key) => (
-                <Table.Row
-                  key={index}
-                  className="w-full dark:border-gray-700 dark:bg-transparent cursor-pointer"
-                >
-                  {expanded ? (
-                    <>
-                      <Table.Cell className="w-1/4 text-center">
-                        {new Date(items.created * 1000).toLocaleString()}
-                      </Table.Cell>
-                      <Table.Cell className="w-1/4 text-center">
-                        {items.amount_paid / 100}
-                      </Table.Cell>
-                      <Table.Cell className="w-1/4 text-center">
-                        {items.lines.data[0].description}
-                      </Table.Cell>
-                      <Table.Cell className="w-1/4 text-center">
-                        <button
-                          className="border rounded-full border-green-500 hover:text-green-500"
-                          onClick={() => {
-                            window.open(items.hosted_invoice_url);
-                          }}
-                        >
-                          <Tooltip
-                            content={"Download"}
-                            className="bg-brand-color text-center"
-                            placement="bottom"
-                          >
-                            <TbDownload className="m-1 transition-fill duration-200 ease-in-out" />
-                          </Tooltip>
-                        </button>
-                      </Table.Cell>
-                    </>
-                  ) : (
-                    <>
-                      <Table.Cell className="w-1/2 text-center">
-                        {new Date(items.created * 1000).toLocaleString()}
-                      </Table.Cell>
-                      <Table.Cell className="w-1/2 text-center">
-                        {items.amount_paid / 100}
-                      </Table.Cell>
-                    </>
-                  )}
-                </Table.Row>
-              ))}
-          </Table.Body>
-        </Table>
-      </div>
+      {invoices && invoices.length !== 0 ? (
+        <>
+          <div className="w-full flex flex-row justify-center items-center gap-4">
+            <Dropdown
+              label="Groups ▼"
+              placement="bottom-start"
+              renderTrigger={() => (
+                <div className="px-2 py-1 rounded-md border border-brand-color cursor-pointer overflow-hidden">
+                  <h1 className="flex items-center justify-center gap-4 duration-100 ease-in text-dark-black dark:text-slate-300 xl:text-base text-sm ">
+                    {status} <span className="text-xs">▼</span>
+                  </h1>
+                </div>
+              )}
+              className="dark:bg-dark-black bg-light-glass backdrop-blur-2xl border-none z-40"
+            >
+              <Dropdown.Item
+                className="dark:text-slate-300 text-light-black hover:text-slate-700"
+                onClick={() => {
+                  handleStatus("paid");
+                }}
+              >
+                Paid
+              </Dropdown.Item>
+              <Dropdown.Item
+                className="dark:text-slate-300 text-light-black hover:text-slate-700"
+                onClick={() => {
+                  handleStatus("open");
+                }}
+              >
+                Open
+              </Dropdown.Item>
+            </Dropdown>
+            <Dropdown
+              label="Groups ▼"
+              placement="bottom-start"
+              renderTrigger={() => (
+                <div className="px-2 py-1 rounded-md border border-brand-color cursor-pointer overflow-hidden">
+                  <h1 className="flex items-center justify-center gap-4 duration-100 ease-in text-dark-black dark:text-slate-300 xl:text-base text-sm ">
+                    {limit} <span className="text-xs">▼</span>
+                  </h1>
+                </div>
+              )}
+              className="dark:bg-dark-black bg-light-glass backdrop-blur-2xl border-none z-40"
+            >
+              <Dropdown.Item
+                className="dark:text-slate-300 text-light-black hover:text-slate-700"
+                onClick={() => {
+                  handleLimit(5);
+                }}
+              >
+                5
+              </Dropdown.Item>
+              <Dropdown.Item
+                className="dark:text-slate-300 text-light-black hover:text-slate-700"
+                onClick={() => {
+                  handleLimit(10);
+                }}
+              >
+                10
+              </Dropdown.Item>
+              <Dropdown.Item
+                className="dark:text-slate-300 text-light-black hover:text-slate-700"
+                onClick={() => {
+                  handleLimit(100);
+                }}
+              >
+                100
+              </Dropdown.Item>
+            </Dropdown>
+          </div>
+          <div className="w-full h-full overflow-auto">
+            <Table hoverable striped>
+              <Table.Head className="w-full">
+                {expanded ? (
+                  <>
+                    <Table.HeadCell className="w-1/4 sticky text-center">
+                      Date
+                    </Table.HeadCell>
+                    <Table.HeadCell className="w-1/4 sticky text-center">
+                      Amount
+                    </Table.HeadCell>
+                    <Table.HeadCell className="w-1/4 sticky text-center">
+                      Description
+                    </Table.HeadCell>
+                    <Table.HeadCell className="w-1/4 sticky text-center">
+                      Invoice
+                    </Table.HeadCell>
+                  </>
+                ) : (
+                  <>
+                    <Table.HeadCell className="w-1/2 sticky text-center">
+                      Date
+                    </Table.HeadCell>
+                    <Table.HeadCell className="w-1/2 sticky text-center">
+                      Amount
+                    </Table.HeadCell>
+                  </>
+                )}
+              </Table.Head>
+              <Table.Body className="divide-y">
+                {invoices &&
+                  invoices.map((items: any, index: Key) => (
+                    <Table.Row
+                      key={index}
+                      className="w-full dark:border-gray-700 dark:bg-transparent cursor-pointer"
+                    >
+                      {expanded ? (
+                        <>
+                          <Table.Cell className="w-1/4 text-center">
+                            {new Date(items.created * 1000).toLocaleString()}
+                          </Table.Cell>
+                          <Table.Cell className="w-1/4 text-center">
+                            {items.amount_paid / 100}
+                          </Table.Cell>
+                          <Table.Cell className="w-1/4 text-center">
+                            {items.lines.data[0].description}
+                          </Table.Cell>
+                          <Table.Cell className="w-1/4 text-center">
+                            <button
+                              className="border rounded-full border-green-500 hover:text-green-500"
+                              onClick={() => {
+                                window.open(items.hosted_invoice_url);
+                              }}
+                            >
+                              <Tooltip
+                                content={"Download"}
+                                className="bg-brand-color text-center"
+                                placement="bottom"
+                              >
+                                <TbDownload className="m-1 transition-fill duration-200 ease-in-out" />
+                              </Tooltip>
+                            </button>
+                          </Table.Cell>
+                        </>
+                      ) : (
+                        <>
+                          <Table.Cell className="w-1/2 text-center">
+                            {new Date(items.created * 1000).toLocaleString()}
+                          </Table.Cell>
+                          <Table.Cell className="w-1/2 text-center">
+                            {items.amount_paid / 100}
+                          </Table.Cell>
+                        </>
+                      )}
+                    </Table.Row>
+                  ))}
+              </Table.Body>
+            </Table>
+          </div>
+        </>
+      ) : (
+        <div className=" w-full h-screen flex flex-col items-center justify-center">
+          <Spinner
+            color="purple"
+            aria-label="Purple spinner example"
+            size="xl"
+          />
+        </div>
+      )}
     </div>
   );
 };
+
 export default CardHistory;
