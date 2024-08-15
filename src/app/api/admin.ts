d76e929ef29ee6c9680726533dc649ab9f07ab10
kbone1997 <doha123khan@gmail.com>
2024-08-15 16:47:44 +0600

    updated homecard loading state

--- src/app/api/admin.ts
+++ src/app/api/admin.ts
@@ -33,3 +33,37 @@ export const addUser = async (
     return error.response;
   }
 };
+
+export const fetchSubAdminList = async (
+  userName: string | null,
+  email: string | null
+) => {
+  const token = typeof window !== "undefined" && localStorage.getItem("token");
+  const parsedToken = token && JSON.parse(token);
+  const userID =
+    typeof window !== "undefined" && localStorage.getItem("userID");
+
+  try {
+    const result = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/company-subadmins`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: parsedToken,
+        },
+        body: JSON.stringify({
+          userID: userID,
+        }),
+      }
+    );
+    if (result) {
+      const responseData = await result.json();
+      return responseData;
+    } else {
+      return null;
+    }
+  } catch (error: any) {
+    return error.response;
+  }
+};
