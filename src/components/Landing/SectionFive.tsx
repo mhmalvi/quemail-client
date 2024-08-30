e7cd560c4fad4316f6f47b2fbd3053b6d9acc4b3
DohaKhanMozlish <doha@quadque.tech>
2024-08-30 12:42:24 +0600

    updated landing ui , updated pricing plans api

--- src/components/Landing/SectionFive.tsx
+++ src/components/Landing/SectionFive.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import Image from "next/image";
 import Images from "../utils/images";
@@ -7,19 +7,23 @@ import { sectionFiveData } from "../utils/staticData";
 const SectionFive = () => {
   return (
     <div className="w-full flex flex-col gap-4 items-center justify-center lg:h-screen h-full lg:py-0 py-16 ">
-      <h1 className="m-0 px-0 py-2 lg:text-2xl text-base text-center flex flex-col lg:gap-2 text-slate-300">
+      <h1 className="flex flex-col text-center bg-clip-text text-transparent bg-gradient-to-r from-brand-color to-brand-color-2 bg-[length:250%_250%] animate-gradient m-0 font-semibold xl:text-5xl lg:text-4xl text-3xl">
         Additional Resources
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-brand-color to-brand-color-2 lg:text-3xl text-xl">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-brand-color to-brand-color-2 bg-[length:250%_250%] animate-gradient lg:text-3xl text-xl">
           for marketing and revenue professionals
         </span>
       </h1>
       <p className="p-0 m-0 lg:w-5/12 w-full text-center text-slate-300">
-        Explore our extensive library of resources to help you get the most out of Quemailer. From
-        detailed guides and how-tos to case studies and industry insights, we provide the knowledge
-        and tools you need to succeed.
+        Explore our extensive library of resources to help you get the most out
+        of Quemailer. From detailed guides and how-tos to case studies and
+        industry insights, we provide the knowledge and tools you need to
+        succeed.
       </p>
-      <Image src={Images && Images.SectionFive} alt="HeroImage"
-        className="xl:w-1/2 lg:w-1/3 md:w-1/2 animate-float" />
+      <Image
+        src={Images && Images.SectionFive}
+        alt="HeroImage"
+        className="xl:w-1/2 lg:w-1/3 md:w-1/2 animate-float"
+      />
       <div className="grid grid-cols-2 items-center text-center justify-center w-full gap-4">
         {sectionFiveData.map((items, index) => {
           return (
