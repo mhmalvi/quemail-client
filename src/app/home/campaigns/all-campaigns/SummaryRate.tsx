a35650979ac5abe3a2868a4313ef485a5ed93850
DohaKhanMozlish <doha@quadque.tech>
2024-09-12 11:12:24 +0600

    Bounced mail functionality is done.

--- src/app/home/campaigns/all-campaigns/SummaryRate.tsx
+++ src/app/home/campaigns/all-campaigns/SummaryRate.tsx
@@ -6,6 +6,8 @@ import { FaPaperPlane } from "react-icons/fa";
 const SummaryRate = () => {
   const [hovered, setHovered] = useState("");
   const campaignItemList = showCampaignStore((state) => state.campaignItemList);
+  console.log('ami from sammaryrate', campaignItemList);
+  
   const deliveryPercentage = useMemo(() => {
     if (
       campaignItemList !== null &&
@@ -15,6 +17,19 @@ const SummaryRate = () => {
     ) {
       return Math.ceil(
         (campaignItemList.delivered / campaignItemList.total) * 100
+      );
+    }
+    return 0;
+  }, [campaignItemList]);
+  const bouncePercentage = useMemo(() => {
+    if (
+      campaignItemList !== null &&
+      campaignItemList.bounce !== null &&
+      campaignItemList.total !== null &&
+      campaignItemList.total !== 0
+    ) {
+      return Math.ceil(
+        (campaignItemList.bounce / campaignItemList.total) * 100
       );
     }
     return 0;
@@ -69,7 +84,7 @@ const SummaryRate = () => {
           </div>
         </div>
         <div className="relative w-2/3 flex flex-col items-end">
-          <h1 className="2xl:text-7xl xl:text-4xl text-4xl text-dark-black dark:text-slate-300 font-semibold px-4 py-2 z-20">
+          <h1 className="2xl:text-6xl xl:text-4xl text-4xl text-dark-black dark:text-slate-300 font-semibold px-4 py-2 z-20">
             90%
           </h1>
           <div className="absolute bottom-0 px-4 flex items-end xl:gap-4 gap-2 z-10">
@@ -118,7 +133,7 @@ const SummaryRate = () => {
           </div>
         </div>
         <div className="relative w-2/3 flex flex-col items-end">
-          <h1 className="2xl:text-7xl xl:text-4xl text-4xl text-dark-black dark:text-slate-300 font-semibold px-4 py-2 z-20">
+          <h1 className="2xl:text-6xl xl:text-4xl text-4xl text-dark-black dark:text-slate-300 font-semibold px-4 py-2 z-20">
             {deliveryPercentage} %
           </h1>
           <div className="absolute bottom-0 px-4 flex items-end xl:gap-4 gap-2 z-10">
@@ -130,6 +145,47 @@ const SummaryRate = () => {
                     "duration-400 ease-in animate-moveRight"
                   }`}
                 />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        onMouseOver={() => {
+          setHovered("Bounced");
+        }}
+        onMouseOut={() => {
+          setHovered("");
+        }}
+        className="flex bg-violet-100 dark:bg-dark-black border border-violet-200 dark:border-light-glass  rounded-2xl w-1/3 h-full overflow-hidden"
+      >
+        <div className="w-2/3 h-full flex flex-col items-start justify-between xl:gap-4 gap-2 p-4">
+          <div className="h-full w-full">
+            <h1 className="text-xl text-brand-color font-semibold">
+              Bounced
+            </h1>
+          </div>
+          <div className="flex items-center justify-center dark:bg-brand-color/20 border-2 border-brand-color h-1/2 rounded-md p-2">
+            <p className="2xl:text-xl xl:text-base text-sm text-green-500">
+              {campaignItemList?.bounce}
+              <span className="text-dark-black dark:text-slate-300">
+                {" "}
+                out of
+              </span>{" "}
+              {campaignItemList?.total}
+            </p>
+          </div>
+        </div>
+        <div className="relative w-2/3 flex flex-col items-end">
+          <h1 className="2xl:text-6xl xl:text-4xl text-4xl text-dark-black dark:text-slate-300 font-semibold px-4 py-2 z-20">
+            {bouncePercentage} %
+          </h1>
+          <div className="absolute bottom-0 px-4 flex items-end xl:gap-4 gap-2 z-10">
+            <div className="p-2">
+              <div className="xl:text-5xl text-2xl bg-gradient-to-r from-brand-color to-brand-color-2 p-4 rounded-full overflow-hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-12 xl:size-10 lg:size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+                </svg>
               </div>
             </div>
           </div>
@@ -162,7 +218,7 @@ const SummaryRate = () => {
           </div>
         </div>
         <div className="relative w-2/3 flex flex-col items-end">
-          <h1 className="2xl:text-7xl xl:text-4xl text-4xl text-dark-black dark:text-slate-300 font-semibold px-4 py-2 z-20">
+          <h1 className="2xl:text-6xl xl:text-4xl text-4xl text-dark-black dark:text-slate-300 font-semibold px-4 py-2 z-20">
             {subscribedPercentage} %
           </h1>
           <div className="absolute bottom-0 px-4 flex items-end xl:gap-4 gap-2 z-10 ">
@@ -233,7 +289,7 @@ const SummaryRate = () => {
           </div>
         </div>
         <div className="relative w-2/3 flex flex-col items-end">
-          <h1 className="2xl:text-7xl xl:text-4xl text-4xl text-dark-black dark:text-slate-300 font-semibold px-4 py-2 z-20">
+          <h1 className="2xl:text-6xl xl:text-4xl text-4xl text-dark-black dark:text-slate-300 font-semibold px-4 py-2 z-20">
             {openPercentage} %
           </h1>
           <div className="absolute bottom-0 px-4 flex items-end xl:gap-4 gap-2 z-10">
